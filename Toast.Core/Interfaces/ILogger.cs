42073e948cd2d77e7107016d5a006ff27bb8d6d8
Marat R. Shakurov <mshakurov@st-hld.ru>
2026-07-06 01:46:52 +0300

    макеты интерфейсов

---
+++ Toast.Core/Interfaces/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toast.Core.Interfaces
+{
+  public interface ILogger
+  {
+    void Info( string message );
+
+    void Warning( string message );
+
+    void Error( Exception exception );
+  }
+}
