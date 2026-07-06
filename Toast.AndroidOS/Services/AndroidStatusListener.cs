9aba5ac99b4142b8bcea026b5ef33b0de6136adf
Marat R. Shakurov <mshakurov@st-hld.ru>
2026-07-06 20:54:31 +0300

    оптимизация уведомлений - раз в секунду, подсчет вызовов уведомлений, добавление в детализацию количества и даты уведомления

--- Toast.AndroidOS/Services/AndroidStatusListener.cs
+++ Toast.AndroidOS/Services/AndroidStatusListener.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,34 +18,76 @@ namespace Toast.AndroidOS.Services
   {
     private readonly Context _context;
     private readonly ILogger? _logger;
+    private readonly Task _notifyTask;
+    private readonly CancellationToken _token;
+    private readonly State _pendingState;
 
-    public AndroidStatusListener( Context context, ILogger? logger )
+    public AndroidStatusListener( Context context, ILogger? logger, CancellationToken token )
     {
       _context = context;
       _logger = logger;
+      _pendingState = new();
+      _token = token;
+      _notifyTask = Task.Factory.StartNew( NotifyLastStatus );
+    }
+
+    private void NotifyLastStatus()
+    {
+      while ( true )
+      {
+        try { _token.WaitHandle.WaitOne( 1000 ); } catch { }
+
+        if ( _token.IsCancellationRequested ) break;
+
+        DoNotify( _pendingState.Get() );
+      }
+    }
+
+    private void DoNotify( State state )
+    {
+      if ( state.PendingStatus == null )
+        return;
+      try
+      {
+        NotificationHelper.UpdateNotification( _context, state.PendingStatus );
+      }
+      catch ( Exception ex )
+      {
+        _logger?.Error( _context, $"Failed to update notification for status (({state.Count}){state.PendingStatus}): {ex.Message},{ex.InnerException?.Message},{ex.InnerException?.InnerException?.Message}" );
+      }
     }
 
     public void ReportStatus( AgentStatus status )
     {
-      try
-      {
-        NotificationHelper.UpdateNotification( _context, status );
-      }
-      catch ( Exception ex )
-      {
-        _logger?.Error( _context, $"Failed to update notification for status (state: {status.State}): {ex.Message},{ex.InnerException?.Message},{ex.InnerException?.InnerException?.Message}" );
-      }
+      //_logger?.Debug( this, $"> {_pendingState.Count}){_pendingState.PendingStatus?.State}|{_pendingState.PendingStatus?.Details} -> ReportStatus({status.State}|{status.Details})" );
+
+      _pendingState.Add( status );
+
+      //_logger?.Debug( this, $"< {_pendingState.Count}){_pendingState.PendingStatus?.State}|{_pendingState.PendingStatus?.Details}" );
     }
 
-    public void ReportStatus( AgentState state )
+    public void ReportStatus( AgentState state ) => ReportStatus( AgentStatus.FromState( state ) );
+
+    class State
     {
-      try
+      object locker = new();
+
+      public long Count { get; private set; }
+      public AgentStatus? PendingStatus { get; private set; }
+
+      public void Add( AgentStatus status )
       {
-        NotificationHelper.UpdateNotification( _context, AgentStatus.FromState( state ) );
+        lock ( locker )
+        {
+          Count += 1;
+          PendingStatus = new AgentStatus { State = status.State, Details = $"({Count} at {status.Timestamp:HH:mm:ss})" + ( status.Details != null ? $" {status.Details}" : string.Empty ), Timestamp = status.Timestamp };
+        }
       }
-      catch ( Exception ex )
+
+      public State Get()
       {
-        _logger?.Error( _context, $"Failed to update notification for state {state}: {ex.Message},{ex.InnerException?.Message},{ex.InnerException?.InnerException?.Message}" );
+        lock ( locker )
+          return new State { Count = Count, PendingStatus = PendingStatus };
       }
     }
   }
